8678c26960cc9490180269785b7683a6b318ef9f
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-02-21 11:50:42 +0530

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,33 @@ function App() {
   const [data, setData] = useState("");
 
   return (
-    <div className="h-[100vh] bg-black flex justify-center items-center">
-      <div className="relative text-amber-600 h-[150px] flex flex-col justify-between items-center w-[120px] bg-amber-200">
+    <>
+   <div className="">
+    <div className=" h-full w-full bg-[#eaded0] fixed  flex  ">
+      <div className="relative text-black  h-full flex flex-col gap-10 justify-center items-center w-[200px] bg-[#f8ebe0] font-customfontinter">
 
-        {/* Moving Box */}
         <div
-          className={`absolute left-0 w-full h-[50px] z-10 transition-all duration-300 bg-white text-center p-2 
-          ${data === "varient" ? "top-0 rounded-b-[4px]" : data === "varient1" ? "top-[50px]" : data === "varient2" ? "top-[100px] rounded-tl-[4px] rounded-tr-[4px]" : "top-0"}`}
-        ></div>
+          className={`absolute left-0 w-full  transition-all duration-300  text-center  
+          ${data === "varient" ? "top-[270px] " : data === "varient1" ? "top-[350px]" : data === "varient2" ? "top-[430px] " : data === "varient3"? "top-[510px]" : "top-[270px]"}`}
+        >
+          <div className="h-full w-full bg-[#eaded0] ">
+            <div className="bg-[#f8ebe0] h-[50px] w-full rounded-br-2xl rounded-bl-2xl "></div>
+          </div>
+          <div className="bg-[#eaded0] h-[50px]  w-full "></div>
+          <div className="h-full w-full bg-[#eaded0] ">
+          <div className="h-[50px] bg-[#f8ebe0]  w-full rounded-tr-2xl rounded-tl-2xl "></div>
 
-        {/* Buttons */}
-        <button onClick={() => setData("varient")} className={`p-2 m-1 transition-all duration-300 ${data === "varient" ? "bg-blue-500 z-50 text-white" : "bg-gray-200"}`}>Home</button>
-        <button onClick={() => setData("varient1")} className={`p-2 m-1 ${data === "varient1" ? "bg-green-500 z-50 text-white" : "bg-gray-200"}`}>Context</button>
-        <button onClick={() => setData("varient2")} className={`p-2 m-1 ${data === "varient2" ? "bg-red-500 z-50 text-white" : "bg-gray-200"}`}>About</button>
-      </div>
-    </div>
+          </div>
+        </div>
+
+        <button onClick={() => setData("varient")} className={`p-2 relative transition-all duration-300 ${data === "varient" ? " z-50 text-black" : "bg-transparent"}`}>Home</button>
+        <button onClick={() => setData("varient1")} className={`p-2 relative ${data === "varient1" ? " z-50 text-black" : "bg-transparent"}`}>About</button> 
+        <button onClick={() => setData("varient2")} className={`p-2 relative ${data === "varient2" ? " z-50 text-black" : "bg-transparent"}`}>Contect</button>
+        <button onClick={() => setData("varient3")} className={`p-2 relative ${data === "varient2" ? " z-50 text-black" : "bg-transparent"}`}>Other</button>
+       
+        
+      </div></div>
+    </div> </>
   );
 }
 
