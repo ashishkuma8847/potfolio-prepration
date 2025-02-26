d3911575d85d03e5c50fb7bd536719347c0e2c37
ashishkuma8847 <ashishkuma8847@gmail.com>
2025-02-26 10:54:26 +0530

    sfd

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   const [data, setData] = useState(
@@ -10,69 +10,79 @@ const Header = () => {
     localStorage.setItem("activeTab", data);
   }, [data]);
 
+  const jsonData = [
+    {
+      name: "Home",
+      to: "/",
+    },
+    {
+      name: "About",
+      to: "/",
+    },
+    {
+      name: "Contact",
+      to: "/",
+    },
+    {
+      name: "Other",
+      to: "/",
+    },
+  ];
+  function handleOver(e) {
+    e.preventDefault();
+    document.querySelectorAll(".nav-link").forEach((nav) => {
+      const pre = nav.previousElementSibling;
+      const nex = nav.nextElementSibling;
+      const cornt = nav;
+      cornt.style.backgroundColor = "#2c2c2c";
+      if (pre) {
+        pre.style.borderBottomRightRadius = ""; // Reset previous element
+      }
+      if (nex) {
+        nex.style.borderTopRightRadius = ""; // Reset next element
+      }
+    });
+    const pre = e.currentTarget.previousElementSibling;
+    const nex = e.currentTarget.nextElementSibling;
+    const cornt = e.currentTarget;
 
+    cornt.style.backgroundColor = "#0D0D0D";
+    if (pre) {
+      pre.style.borderBottomRightRadius = "10px";
+    }
+    if (nex) {
+      nex.style.borderTopRightRadius = "10px";
+    }
+  }
+  function handleLeave(e) {
+    const pre = e.currentTarget.previousElementSibling;
+    const nex = e.currentTarget.nextElementSibling;
+
+    if (pre) {
+      pre.style.borderBottomRightRadius = ""; // Reset to default
+    }
+    if (nex) {
+      nex.style.borderTopRightRadius = ""; // Reset to default
+    }
+  }
 
   return (
     <>
       <div className="h-full bg-[#2c2c2c] items-center justify-center fixed flex">
-        <div className="relative text-white h-[300px] flex flex-col gap-10 justify-center items-center w-[200px] bg-[#2c2c2c] font-customfontinter">
-          <div
-            className={`absolute left-0 w-full transition-all duration-300 text-center  
-          ${
-            data === "varient"
-              ? "top-[-45px]"
-              : data === "varient1"
-              ? "top-[35px]"
-              : data === "varient2"
-              ? "top-[115px]"
-              : data === "varient3"
-              ? "top-[195px]"
-              : ""
-          }`}
-          >
-            <div className="h-full w-full bg-[#0D0D0D]">
-              <div className="bg-[#2c2c2c] h-[50px] w-full rounded-br-2xl rounded-bl-2xl"></div>
-            </div>
-            <div className="bg-[#0D0D0D] h-[50px] w-full"></div>
-            <div className="h-full w-full bg-[#0D0D0D]">
-              <div className="h-[50px] bg-[#2c2c2c] w-full rounded-tr-2xl rounded-tl-2xl"></div>
-            </div>
-          </div>
-          <Link to={"/"}>
-            <button
-              onClick={() => setData("varient")}
-              className={`p-2 relative transition-all duration-300 ${
-                data === "varient" ? "z-50 " : "bg-transparent"
-              }`}
+        <div className="relative text-white  flex flex-col justify-center items-center w-[200px] bg-[#0D0D0D]  font-customfontinter">
+          <div className="bg-[#2c2c2c] w-full h-3.5"></div>
+          {jsonData.map((item, index) => (
+            <NavLink
+              to={item.to}
+              onClick={handleOver}
+              // onMouseLeave={handleLeave}
+              key={index}
+              className="nav-link py-4 text-xl transition-all duration-200 ease-in  w-full text-center bg-[#2c2c2c]"
             >
-              Home
-            </button>
-          </Link>
-
-          <button
-            onClick={() => setData("varient1")}
-            className={`p-2 relative ${
-              data === "varient1" ? "z-50 " : "bg-transparent"
-            }`}
-          >
-            About
-          </button>
-          <button
-            onClick={() => setData("varient2")}
-            className={`p-2 relative ${
-              data === "varient2" ? "z-50 " : "bg-transparent"
-            }`}
-          >
-            Contact
-          </button>
-          <button
-            onClick={() => setData("varient3")}
-            className={`p-2 relative ${
-              data === "varient3" ? "z-50 " : "bg-transparent"
-            }`}
-          >
-            Other
-          </button>
+              {item.name}
+            </NavLink>
+          ))}
+          <div className="bg-[#2c2c2c] w-full h-3.5"></div>
         </div>
       </div>
     </>
---
+++ src/components/common/Header11.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [data, setData] = useState(
+    () => localStorage.getItem("activeTab") || "varient"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("activeTab", data);
+  }, [data]);
+
+
+
+  return (
+    <>
+      <div className="h-full bg-[#2c2c2c] items-center justify-center fixed flex">
+        <div className="relative text-white h-[300px] flex flex-col gap-10 justify-center items-center w-[200px] bg-[#2c2c2c] font-customfontinter">
+          <div
+            className={`absolute left-0 w-full transition-all duration-300 text-center  
+          ${
+            data === "varient"
+              ? "top-[-45px]"
+              : data === "varient1"
+              ? "top-[35px]"
+              : data === "varient2"
+              ? "top-[115px]"
+              : data === "varient3"
+              ? "top-[195px]"
+              : ""
+          }`}
+          >
+            <div className="h-full w-full bg-[#0D0D0D]">
+              <div className="bg-[#2c2c2c] h-[50px] w-full rounded-br-2xl rounded-bl-2xl"></div>
+            </div>
+            <div className="bg-[#0D0D0D] h-[50px] w-full"></div>
+            <div className="h-full w-full bg-[#0D0D0D]">
+              <div className="h-[50px] bg-[#2c2c2c] w-full rounded-tr-2xl rounded-tl-2xl"></div>
+            </div>
+          </div>
+          <Link to={"/"}>
+            <button
+              onClick={() => setData("varient")}
+              className={`p-2 relative transition-all duration-300 ${
+                data === "varient" ? "z-50 " : "bg-transparent"
+              }`}
+            >
+              Home
+            </button>
+          </Link>
+
+          <button
+            onClick={() => setData("varient1")}
+            className={`p-2 relative ${
+              data === "varient1" ? "z-50 " : "bg-transparent"
+            }`}
+          >
+            About
+          </button>
+          <button
+            onClick={() => setData("varient2")}
+            className={`p-2 relative ${
+              data === "varient2" ? "z-50 " : "bg-transparent"
+            }`}
+          >
+            Contact
+          </button>
+          <button
+            onClick={() => setData("varient3")}
+            className={`p-2 relative ${
+              data === "varient3" ? "z-50 " : "bg-transparent"
+            }`}
+          >
+            Other
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
